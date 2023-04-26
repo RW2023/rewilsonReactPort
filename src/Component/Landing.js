755c64a81216b055018e163bf45f0b25d5fc35b7
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-04-26 00:17:23 -0400

    add noopener no referer

--- src/Component/Landing.js
+++ src/Component/Landing.js
@@ -10,27 +10,27 @@ class Landing extends React.Component {
                 <div className="intro" id="intro">Hello, I'm Ryan!</div>
                 <div className="tagline" id="tagline">All-Star Rookie | Code Junkie | Linux CL Hacker |  Junior  Dev</div>
                 <div className="icons-social" id='LINKS'>
-                    <a target="_blank" href="https://github.com/RW2023">
+                    <a target="_blank" rel="noopener noreferrer" href="https://github.com/RW2023">
                         <FontAwesomeIcon className="social-icon" icon={faGithub} title="Github" />
                         <span className="sr-only">Github</span>
                     </a>
-                    <a target="_blank" href="https://twitter.com/ryanwilson2009">
+                    <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/ryanwilson2009">
                         <FontAwesomeIcon className="social-icon" icon={faTwitter} title="Twitter" />
                         <span className="sr-only">Twitter</span>
                     </a>
-                    <a target="_blank" href="https://dev.to/rw2023">
+                    <a target="_blank" rel="noopener noreferrer" href="https://dev.to/rw2023">
                         <FontAwesomeIcon className="social-icon" icon={faDev} title="DEV Community" />
                         <span className="sr-only">DEV Community</span>
                     </a>
-                    <a target="_blank" href="https://stackoverflow.com/users/20805392/rew">
+                    <a target="_blank" rel="noopener noreferrer" href="https://stackoverflow.com/users/20805392/rew">
                         <FontAwesomeIcon className="social-icon" icon={faStackOverflow} title="StackOverflow" />
                         <span className="sr-only">StackOverflow</span>
                     </a>
-                    <a target="_blank" href="https://www.linkedin.com/in/ryan-e-wilson/">
+                    <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/ryan-e-wilson/">
                         <FontAwesomeIcon className="social-icon" icon={faLinkedin} title="LinkedIn" />
                         <span className="sr-only">LinkedIn</span>
                     </a>
-                    <a target="_blank" href='https://www.instagram.com/ryanwilsonimages/'>
+                    <a target="_blank" rel="noopener noreferrer" href='https://www.instagram.com/ryanwilsonimages/'>
                         <FontAwesomeIcon className="social-icon" icon={faInstagram} />
                         <span className="sr-only">Instagram</span>
                         </a>
